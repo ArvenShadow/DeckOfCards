936fe3c029980c3cb66a5db8fbb0284a8672deee
ArvenShadow <ole.markus@prolek.no>
2025-03-13 15:39:32 +0100

    Found a javafx card face creator online

--- src/main/java/edu/ntnu/idi/idatt2003/cardsfx/Rank.java
+++ src/main/java/edu/ntnu/idi/idatt2003/cardsfx/Rank.java
@@ -1,4 +1,29 @@
 package edu.ntnu.idi.idatt2003.cardsfx;
 
 public enum Rank {
+
+  ACE( "A"),
+  _2( "2"),
+  _3("3"),
+  _4("4"),
+  _5("5"),
+  _6("6"),
+  _7("7"),
+  _8("8"),
+  _9("9"),
+  _10("10"),
+  JACK("J"),
+  QUEEN("Q"),
+  KING("K")
+  ;
+
+  String name;
+
+  Rank( String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
